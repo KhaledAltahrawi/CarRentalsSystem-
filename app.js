f89f1029707bc6ff4a45d45c931a07f6ce0a9a94
mbobat1 <mbobat1@my.centennialcollege.ca>
2024-07-15 17:11:37 -0400

    added server root message + changed database url

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/db');
-require('dotenv').config();
+const express = require("express");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,8 +11,11 @@ connectDB();
 app.use(express.json());
 
 // Define Routes
-app.use('/api', require('./routes/userRoutes'));
-
+app.use("/api", require("./routes/userRoutes"));
+// root
+app.get("/", (req, res) => {
+ res.json({ message: "Welcome to the rental car application." });
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

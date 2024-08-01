7e5dfe1a62a283e8540e669330ea3be98e13a174
shrawan-sk <shrawankhanal6@gmail.com>
2024-08-01 05:19:54 -0400

    car

---
+++ routes/carRoutes
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getCars } = require('../controllers/carController');
+const router = express.Router();
+
+router.get('/', getCars);
+
+module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,45 @@
+// routes/userRoutes.js
 const express = require('express');
-const { registerUser, getUsers, getUserById, updateUser, deleteUser, signInUser, signOutUser } = require('../controllers/userController');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
 const router = express.Router();
 
-// Create a new user
-router.post('/users', registerUser);
-
-// List all users
-router.get('/users', getUsers);
-
-// Fetch a user by ID
-router.get('/user/:id', getUserById);
-
-// Update a user by ID
-router.put('/users/:id', updateUser);
-
-// Delete a user by ID
-router.delete('/users/:id', deleteUser);
-
-// User sign in
-router.post('/auth/signin', signInUser);
-
-// User sign out
-router.post('/auth/signout', signOutUser);
+router.post('/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+        user = new User({
+            name,
+            email,
+            password
+        });
+        await user.save();
+        res.status(201).json({ msg: 'User registered successfully' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+        res.status(200).json({ msg: 'User signed in successfully' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});

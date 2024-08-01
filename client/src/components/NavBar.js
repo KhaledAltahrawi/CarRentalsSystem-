bd28545feff97c879191f2d85faca631e7319804
shrawan-sk <shrawankhanal6@gmail.com>
2024-08-01 05:11:59 -0400

    Components

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,23 @@
+// client/src/components/NavBar.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'; // Make sure this path is correct
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <img src="/logo.png" alt="Car Rental Logo" className="logo" />
+        <span>Car Rental</span>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/signup">Sign Up</Link></li>
+        <li><Link to="/signin">Sign In</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/SignIn.js
@@ -0,0 +1,41 @@
+// src/components/SignIn.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SignIn = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const handleChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try {
+            const user = {
+                email,
+                password
+            };
+            await axios.post('http://localhost:5000/api/users/signin', user);
+            console.log('User signed in');
+        } catch (err) {
+            console.error(err.response.data);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="email" name="email" value={email} onChange={handleChange} required placeholder="Email" />
+                <input type="password" name="password" value={password} onChange={handleChange} required placeholder="Password" />
+                <button type="submit">Sign In</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignIn;

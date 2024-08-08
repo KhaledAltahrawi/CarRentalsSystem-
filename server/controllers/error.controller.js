eeda0e2b7e2ef7bf30c0a861116f08a6e2c6ebbf
shrawan-sk <shrawankhanal6@gmail.com>
2024-08-08 17:27:31 -0400

    Minor issues fixed

--- server/controllers/error.controller.js
+++ server/controllers/error.controller.js
@@ -1,7 +1,3 @@
-// error.controller.js
-// Import any necessary modules or dependencies
-// Example: const SomeModule = require('some-module');
-// Define your controller function
 function handleError(req, res) {
   // Your code to handle the error
 }
